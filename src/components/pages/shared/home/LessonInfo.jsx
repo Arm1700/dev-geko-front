589e203e0f27cfddcb4bb10e624474db95cb9fce
Arm1700 <bukbok1@gmail.com>
2024-04-15 16:26:47 +0400

    Remove all content

--- src/components/pages/shared/home/LessonInfo.jsx
+++
@@ -1,9 +0,0 @@
-export default function LessonInfo({Icon,title,count}){
-    return(
-        <article className="flex p-5 hover:text-secondary hover:border-secondary  transition-colors duration-300 gap-2 border-b text-white flex-col items-center">
-          <Icon className="text-7xl" />
-          <span className="text-xl font-bold">{count}</span>
-          <span>{title}</span>
-        </article>
-    )
-}
