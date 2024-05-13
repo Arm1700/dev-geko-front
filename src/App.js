8a4bc5f63a532a50099b693a4b8e7d3eb1279233
Arm1700 <bukbok1@gmail.com>
2024-05-13 19:06:42 +0400

    notFound

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import './App.css'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import { Route, Routes } from 'react-router-dom'
-import { routesArray } from './entities/routesArray'
-import PageUpButton from './components/pages/shared/PageUpButton'
-import CoursePage from './components/pages/courses/[id]'
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { routesArray } from './entities/routesArray';
+import PageUpButton from './components/pages/shared/PageUpButton';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Error404 from "./components/pages/shared/Error";
 
 function App() {
-  return (
-    <div className="App">
-      <PageUpButton />
-      <Header />
-
-      <Routes>
-        {routesArray.map(route => (
-          <Route
-            key={route.id}
-            path={route.path}
-            element={<route.component />}
-            exact
-          />
-        ))}
-        <Route key={100} path={'/courses/:id'} element={<CoursePage />} exact />
-      </Routes>
-
-      <Footer />
-    </div>
-  )
+    return (
+        <div className="App">
+            <PageUpButton />
+            <Header />
+            <Routes>
+                {routesArray.map(route => (
+                    <Route
+                        key={route.id}
+                        path={route.path}
+                        element={<route.component />}
+                        exact
+                    />
+                ))}
+                <Route path="*" element={<Error404 />} /> {/* Добавляем маршрут для страницы NotFound */}
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/pages/shared/Error.jsx
+++ src/components/pages/shared/Error.jsx
@@ -1,25 +1,45 @@
 // Error404.jsx
-import React from 'react';
-import {  useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const Error404 = () => {
-  const nav = useNavigate();
+    const nav = useNavigate();
+    const [isHovered, setIsHovered] = useState(false);
+    const goBack = () => {
+        nav('');
+    };
 
-  const goBack = () => {
-    nav('/course');
-  };
-
-  return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h1 className="text-4xl font-bold mb-4">404 - Not Found</h1>
-      <button
-        className="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded"
-        onClick={goBack}
-      >
-        Go Back
-      </button>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center w-full">
+            <h1 className="text-3xl font-roboto-slab font-bold mb-4 self-start px-20 pt-7 text-primaryDark">404
+                Page</h1>
+            <div className="flex p-20 justify-center gap-10 items-center">
+                <img className="w-[480px] h-[400px]"
+                     src="https://eduma.thimpress.com/wp-content/uploads/2022/07/404-error.png" alt=""/>
+                <div className="w-[430px]">
+                    <p className="text-7xl font-roboto-slab">
+                        404
+                        <span className="text-primary text-bold"> ERROR!</span>
+                    </p>
+                    <p className="text-custom-15 text-color60">
+                        Sorry, we can't find the page you are looking for. Please go to
+                        <span className="text-primary cursor-pointer font-bold"
+                              onMouseEnter={() => setIsHovered(true)}
+                              onMouseLeave={() => setIsHovered(false)}
+                              onClick={goBack}
+                              style={{borderBottom: isHovered ? '2px solid #FFB606' : 'none'}}
+                        > Home.</span>
+                    </p>
+                </div>
+                {/*<button*/}
+                {/*    className="bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded"*/}
+                {/*    onClick={goBack}*/}
+                {/*>*/}
+                {/*    Go Back*/}
+                {/*</button>*/}
+            </div>
+        </div>
+    );
 };
 
 export default Error404;
