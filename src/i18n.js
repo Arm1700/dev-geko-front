73678e16b8cd94da8355c399126743717c532192
Arm1700 <115033633+Arm1700@users.noreply.github.com>
2024-09-09 13:38:27 +0400

    i18n connect django

--- src/i18n.js
+++ src/i18n.js
@@ -24,7 +24,7 @@ i18n
         //     ru
         // },
         backend: {
-            loadPath: 'http://127.0.0.1:8000/api/translations/{{lng}}/',
+            loadPath: 'https://dev.gekoeducation.com//api/translations/{{lng}}/',
         },
         cache: {
             enabled: true,
